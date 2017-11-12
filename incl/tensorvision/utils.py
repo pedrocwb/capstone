798aaff50f91cb8b1a4f32a2a66e6948080a5333
pmmn11 <pedromartins.cwb@gmail.com>
2017-11-12 03:06:02 -0200

    run_test update

--- incl/tensorvision/utils.py
+++ incl/tensorvision/utils.py
@@ -1,5 +1,7 @@
 """Utility functions for TensorVision."""
 """Utility functions for TensorVision."""
+
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
